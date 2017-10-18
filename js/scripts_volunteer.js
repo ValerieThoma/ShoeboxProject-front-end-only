00cdeaf7f3db44f604a8105755eb2f3a02c698df
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-10-18 16:03:21 -0400

    Submit button to change colors.

--- js/scripts_volunteer.js
+++ js/scripts_volunteer.js
@@ -1,19 +1,31 @@
 $(document).ready(()=>{
-	fieldsFull = false;
-	$('.sign-up-form').change(function(){
-		$('form-wrapper input').each(function(){
-			var userInput = $(this).val();
+	var fieldsFull = false;
+	var count = 0;
+	$('.sign-up-form').change(()=>{
+		count++;
+		// $('form-wrapper input').each(function(){
+		// 	var userInput = $(this).val();
 
-			if(userInput === 'j'){
-				fieldsFull = true;
-				console.log("True!")
+		// 	if(userInput == 'j'){
+		// 		fieldsFull = true;
+		// 		console.log("True!")
+		// 	}
+		// });
+
+		// if(fieldsFull){
+		// 	$('.submit').removeClass('btn-warning');
+		// }else{
+		// 	$('.submit').addClass('btn-success');
+		// }
+		// $('.agree').change(()=>{
+		// 	console.log("Ready!")
+			if(count >= 5){
+				$('.submit').removeClass('btn-warning');
+				$('.submit').addClass('btn-success');
 			}
-		});
 
-		if(fieldsFull){
-			$('.submit').addClass('btn-success');
-		}else{
-			$('.submit').addClass('btn-warning');
-		}
+
+		// });
+		
 	});
 });
