c469e2c09c68cf340cc01a119470b4f677da7dff
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-10-18 22:27:20 -0400

    Password match

--- js/scripts_volunteer.js
+++ js/scripts_volunteer.js
@@ -19,12 +19,38 @@ $(document).ready(()=>{
 		// }
 		// $('.agree').change(()=>{
 		// 	console.log("Ready!")
-			if(count >= 5){
-				$('.submit').removeClass('btn-warning');
-				$('.submit').addClass('btn-success');
-				$('.submit').attr('value', "I'm ready!")
-			}
+			var password = $('.password').val();
+			var passwordConfirm = $('.password-confirm').val();
 
+			$('.agree').change(()=>{
+				if(count >= 5){
+					$('.submit').removeClass('btn-warning');
+					$('.submit').addClass('btn-success');
+					$('.submit').attr('value', "I'm ready!")
+				}	
+			});
+			
+			// $('.password-confirm').change(()=>{
+			// 	if(password != passwordConfirm){
+			// 		console.log(password);
+			// 		console.log(passwordConfirm);
+			// 		$('.password-error').html("Your passwords do not match.");
+			// 	}
+
+
+			// });
+
+			$('.sign-up-form').submit((event)=>{
+				event.preventDefault();
+				var password = $('.password').val();
+				var passwordConfirm = $('.password-confirm').val();
+				if(password != passwordConfirm){
+					// console.log(password);
+					// console.log($('.password-confirm').val());
+					$('.password-error').html("Your passwords do not match.");
+				}
+				// console.log(passwordConfirm);
+			});
 
 		// });
 		
