0106a585bc59d89a90c1621c53a88ebe28b6a89a
Allysonc1 <allyson.conrad@gmail.com>
2017-10-24 11:49:17 -0400

    readme

--- js/scripts_login.js
+++ js/scripts_login.js
@@ -106,37 +106,24 @@ $(document).ready(()=>{
 		}
 
 		var numUsers = localStorage.getItem('users-signedup')
-		console.log("numUsers", numUsers)
 		var enteredEmail = $('.email').val();
-		console.log("enteredEmail", enteredEmail)
 		var enteredPassword = $('.password').val();
-		console.log("enteredPassword", enteredPassword)
 
 		for (i=1; i <= numUsers; i++){
 
-			// userObj.userType = localStorage.getItem('type'+i)
 			userObj.passwd = localStorage.getItem('password'+i)
 			userObj.userEmail = localStorage.getItem('userEmail'+i)
-			// userObj.fullName = localStorage.getItem('fullName'+i)
-			// userObj.userPhone = localStorage.getItem('userPhone'+i)
-			// userObj.signupDate = localStorage.getItem('signupDate'+i)
-
-			console.log("userObj.userEmail", userObj.userEmail)
-			console.log("userObj.passwd", userObj.passwd)
 
 			if (enteredEmail == userObj.userEmail){
 
 				if (enteredPassword != userObj.passwd){
-					console.log(" != timesSubmitIsRun", timesSubmitIsRun)
 					$('.password-error').html("Incorrect password.");
 				}else{
-					console.log(" == timesSubmitIsRun", timesSubmitIsRun)
 					window.location.href = "user_home.html"
 					break;
 				}
 
 			}else{
-				console.log("pswd error", enteredEmail, userObj.userEmail)
 				$('.password-error').html("Unknown or incorrect email address");
 			}
 		}
