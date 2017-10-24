71d746764ae1f0ca6d2c8560ca61bf636c289483
Valerie Jane <valeriejanethoma@gmail.com>
2017-10-24 07:46:41 -0400

    Added carousel and modal to user_home.html but only after endless, tortuous struggle with css. Need hugs.

--- js/user_home.js
+++ js/user_home.js
@@ -1,5 +1,35 @@
-$(document).ready(()=>{
-	// console.log("I'm here!");
-	// var userName = localStorage.getItem('userName');
-	// $('.welcome-message').html(`<h1>Welcome, ${userName}!`);
+$(document).ready(function() {
+       
+   /* activate the carousel */
+   $("#modal-carousel").carousel({interval:false});
+
+   /* change modal title when slide changes */
+   $("#modal-carousel").on("slid.bs.carousel",       function () {
+        $(".modal-title")
+        .html($(this)
+        .find(".active img")
+        .attr("title"));
+   });
+
+   /* when clicking a thumbnail */
+   $(".row .thumbnail").click(function(){
+    var content = $(".carousel-inner");
+    var title = $(".modal-title");
+  
+    content.empty();  
+    title.empty();
+  
+  	var id = this.id;  
+     var repo = $("#img-repo .item");
+     var repoCopy = repo.filter("#" + id).clone();
+     var active = repoCopy.first();
+  
+    active.addClass("active");
+    title.html(active.find("img").attr("title"));
+  	content.append(repoCopy);
+
+    // show the modal
+  	$("#modal-gallery").modal("show");
+  });
+
 });
