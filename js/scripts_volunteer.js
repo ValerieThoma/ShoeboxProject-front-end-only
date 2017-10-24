48cd0a87b125dea86f31c2a18787903dd9a10af0
Allysonc1 <allyson.conrad@gmail.com>
2017-10-24 10:53:07 -0400

    add pswd/email data to login to volunteer page

--- js/scripts_volunteer.js
+++ js/scripts_volunteer.js
@@ -41,21 +41,83 @@ $(document).ready(()=>{
 
 			// });
 
-			$('.vol-sign-up-form').submit((event)=>{
-				event.preventDefault();
-				var password = $('.password').val();
-				var passwordConfirm = $('.password-confirm').val();
-				if(password != passwordConfirm){
-					// console.log(password);
-					// console.log($('.password-confirm').val());
-					$('.password-error').html("Your passwords do not match.");
-				}else{
-					localStorage.setItem('vol-password', password);
-				}
-				// console.log(passwordConfirm);
-			});
+			// $('.vol-sign-up-form').submit((event)=>{
+			// 	event.preventDefault();
+			// 	var password = $('.password').val();
+			// 	var passwordConfirm = $('.password-confirm').val();
+			// 	if(password != passwordConfirm){
+			// 		// console.log(password);
+			// 		// console.log($('.password-confirm').val());
+			// 		$('.password-error').html("Your passwords do not match.");
+			// 	}else{
+			// 		localStorage.setItem('vol-password', password);
+			// 	}
+			// 	// console.log(passwordConfirm);
+			// });
+			// store input data from user signup screen
 
-		// });
-		
 	});
+	
+	var userObj = {
+		userType :  [],
+		fullName : [],
+		userPhone : [],
+		userEmail : [],
+		passwd : [],
+		signupDate : []
+	}
+	userObj.signupDate = new Date();
+
+	$('.vol-sign-up-form').submit((event)=>{
+		event.preventDefault();
+
+		var password = $('.password').val();
+		var passwordConfirm = $('.password-confirm').val();
+
+		var numUsers = localStorage.getItem('users-signedup')
+		
+		if(password != passwordConfirm){
+			console.log(password);
+			console.log($('.password-confirm').val());
+			$('.password-error').html("Your passwords do not match.");
+		}else{
+			console.log("passwords match")
+			
+			if(numUsers == null){
+				numUsers = 0;
+				console.log("numUsers is zero",numUsers)
+			}else{
+				numUsers++;
+			}
+			console.log("numUsers ",numUsers)
+			localStorage.setItem('users-signedup',numUsers)
+
+
+			userObj.userType= 'Volunteer';
+			userObj.fullName = $('.full-name').val();
+			userObj.userPhone = $('.phone').val();
+			userObj.userEmail = $('.email').val();
+			userObj.passwd = $('.password').val();
+			
+
+			console.log("fullName ", userObj.fullName)
+			console.log("userPhone ", userObj.userPhone);
+			console.log("userEmail ", userObj.userEmail);
+			console.log("password ", userObj.passwd);
+
+
+			localStorage.setItem("userType"+numUsers, userObj.userType);
+			localStorage.setItem("password"+numUsers, userObj.passwd);
+			localStorage.setItem("fullName"+numUsers, userObj.fullName);
+			localStorage.setItem("userEmail"+numUsers, userObj.userEmail);
+			localStorage.setItem("userPhone"+numUsers, userObj.userPhone);
+			localStorage.setItem("signupDate"+numUsers, userObj.signupDate);
+
+			window.location.href = "user_home.html"
+			
+		}
+
+	});
+	
+
 });
